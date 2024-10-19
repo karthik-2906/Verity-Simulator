c5001d38b5b0268233bbc0d70875ad001b2c5989
Karthik Sivakumar <karthik29062000@gmail.com>
2024-10-19 14:34:21 +0530

    Added random statue shapes logic

--- js/script.js
+++ js/script.js
@@ -5,12 +5,17 @@
             this.randomiseBtn = document.querySelector('.verity-container__controls-randomise');
             this.lettersCheckbox = document.getElementById('letters');
             this.calloutsArr = ['C', 'T', 'S'];
+            this.statuesArr = ['C', 'C', 'T', 'T', 'S', 'S'];
 
             this.shapes = [
                 { src: 'assets/circle.svg', letter: 'C' },
                 { src: 'assets/triangle.svg', letter: 'T' },
                 { src: 'assets/square.svg', letter: 'S' }
             ];
+
+            this.leftStatue = document.querySelector(".verity-container__statue-left");
+            this.midStatue = document.querySelector(".verity-container__statue-mid");
+            this.rightStatue = document.querySelector(".verity-container__statue-right");
 
             this.shapeHeldTitle = document.getElementsByClassName("verity-container__shape-held-title")[0];
             this.knights = document.getElementsByClassName("verity-container__enemies-knight");
@@ -115,13 +120,135 @@
                 }
             });
 
-            console.log(this.calloutsArr);
             this.randomiseStatues();
         }
 
         randomiseStatues() {
-            console.log(this.calloutsArr);
-            
+            let possibility = (Math.floor(Math.random() * 5) + 1);
+
+            if (possibility == 1) {
+                this.statuesArr.sort((a, b) => {
+                    return this.calloutsArr.indexOf(a) - this.calloutsArr.indexOf(b);
+                });
+
+                console.log("First possibility", this.statuesArr);
+                this.populateStatues();
+            }
+
+            if (possibility == 2 || possibility == 3) {
+                const randomShape = this.calloutsArr[Math.floor(Math.random() * this.calloutsArr.length)];
+                const randomShapeIndex = this.calloutsArr.indexOf(randomShape);
+
+                const newStatuesArr = [randomShape, randomShape];
+                const remainingElements = this.statuesArr.filter(item => item !== randomShape);
+
+                const shuffleArray = (arr) => {
+                    for (let i = arr.length - 1; i > 0; i--) {
+                        const j = Math.floor(Math.random() * (i + 1));
+                        [arr[i], arr[j]] = [arr[j], arr[i]];
+                    }
+                };
+
+                let finalStatuesArr;
+
+                if (randomShapeIndex === 0) {
+                    do {
+                        shuffleArray(remainingElements);
+                        finalStatuesArr = newStatuesArr.concat(remainingElements);
+
+                    } while (finalStatuesArr[2] === finalStatuesArr[3]);
+
+                    this.statuesArr = finalStatuesArr;
+                    console.log("Second possibility", this.statuesArr);
+                    this.populateStatues();
+
+                } else if (randomShapeIndex === 1) {
+                    do {
+                        shuffleArray(remainingElements);
+                        let arr1 = remainingElements.slice(0, 2);
+                        let arr2 = remainingElements.slice(2, 4);
+                        finalStatuesArr = arr1.concat(newStatuesArr, arr2);
+
+                    } while (finalStatuesArr[0] === finalStatuesArr[1]);
+
+                    this.statuesArr = finalStatuesArr;
+                    console.log("Second possibility", this.statuesArr);
+                    this.populateStatues();
+
+                } else if (randomShapeIndex == 2) {
+                    do {
+                        shuffleArray(remainingElements);
+                        finalStatuesArr = remainingElements.concat(newStatuesArr);
+
+                    } while (finalStatuesArr[0] === finalStatuesArr[1]);
+
+                    this.statuesArr = finalStatuesArr;
+                    console.log("Second possibility", this.statuesArr);
+                    this.populateStatues();
+                }
+            }
+
+            if (possibility == 4 || possibility == 5) {
+                console.log("Third possibility");
+                this.populateStatues();
+            }
+
+        }
+
+        populateStatues() {
+            this.leftStatue.setAttribute('statue-shape', this.statuesArr[0] + this.statuesArr[1]);
+            this.midStatue.setAttribute('statue-shape', this.statuesArr[2] + this.statuesArr[3]);
+            this.rightStatue.setAttribute('statue-shape', this.statuesArr[4] + this.statuesArr[5]);
+
+            this.updateStatueShape();
+        }
+
+        updateStatueShape() {
+            let leftStatueShapes = this.leftStatue.getAttribute('statue-shape');
+            let midStatueShapes = this.midStatue.getAttribute('statue-shape');
+            let rightStatueShapes = this.rightStatue.getAttribute('statue-shape');
+
+            if (leftStatueShapes == "CC") {
+                this.leftStatue.children[0].src = "assets/sphere.svg";
+            } else if (leftStatueShapes == "SS") {
+                this.leftStatue.children[0].src = "assets/cube.svg";
+            } else if (leftStatueShapes == "TT") {
+                this.leftStatue.children[0].src = "assets/pyramid.svg";
+            } else if (leftStatueShapes == "SC" || leftStatueShapes == "CS") {
+                this.leftStatue.children[0].src = "assets/cylinder.svg";
+            } else if (leftStatueShapes == "TS" || leftStatueShapes == "ST") {
+                this.leftStatue.children[0].src = "assets/prism.png";
+            } else if (leftStatueShapes == "CT" || leftStatueShapes == "TC") {
+                this.leftStatue.children[0].src = "assets/cone.svg";
+            }
+
+            if (midStatueShapes == "CC") {
+                this.midStatue.children[0].src = "assets/sphere.svg";
+            } else if (midStatueShapes == "SS") {
+                this.midStatue.children[0].src = "assets/cube.svg";
+            } else if (midStatueShapes == "TT") {
+                this.midStatue.children[0].src = "assets/pyramid.svg";
+            } else if (midStatueShapes == "SC" || midStatueShapes == "CS") {
+                this.midStatue.children[0].src = "assets/cylinder.svg";
+            } else if (midStatueShapes == "TS" || midStatueShapes == "ST") {
+                this.midStatue.children[0].src = "assets/prism.png";
+            } else if (midStatueShapes == "CT" || midStatueShapes == "TC") {
+                this.midStatue.children[0].src = "assets/cone.svg";
+            }
+
+            if (rightStatueShapes == "CC") {
+                this.rightStatue.children[0].src = "assets/sphere.svg";
+            } else if (rightStatueShapes == "SS") {
+                this.rightStatue.children[0].src = "assets/cube.svg";
+            } else if (rightStatueShapes == "TT") {
+                this.rightStatue.children[0].src = "assets/pyramid.svg";
+            } else if (rightStatueShapes == "SC" || rightStatueShapes == "CS") {
+                this.rightStatue.children[0].src = "assets/cylinder.svg";
+            } else if (rightStatueShapes == "TS" || rightStatueShapes == "ST") {
+                this.rightStatue.children[0].src = "assets/prism.png";
+            } else if (rightStatueShapes == "CT" || rightStatueShapes == "TC") {
+                this.rightStatue.children[0].src = "assets/cone.svg";
+            }
         }
 
         handleLetterCheckbox() {
